0d9ed600cb7dc8372758262fd15ccf3213db1c17
ricardo.machado <ricardo.machado@zup.com.br>
2020-05-25 17:05:21 -0300

    Add provider service

--- src/helping_hands/provider/service.clj
+++ src/helping_hands/provider/service.clj
@@ -1,3 +1,44 @@
-(ns helping-hands.provider.service)
+(ns helping-hands.provider.service
+  (:require [io.pedestal.http.body-params :as body-params]
+            [io.pedestal.http :as http]
+            [helping-hands.service :as generic-interceptors]
+            [helping-hands.provider.core :as core]))
 
-(def routes #{[""]})
+(def common-interceptors [(body-params/body-params) http/html-body])
+
+(def gen-events
+  {:name ::events
+
+   :enter
+         (fn [context]
+           (if (:response context)
+             context
+             (assoc context :response {:status 200 :body "SUCCESS"})))
+
+   :error
+         (fn [context ex-info]
+           (assoc context
+             :response {:status 500
+                        :body (.getMessage ex-info)}))})
+
+(def routes #{["/providers/:id"
+               :get (conj common-interceptors `generic-interceptors/auth `core/validate-id `core/upsert-provider `gen-events)
+               :route-name :provider-get]
+              ["/providers/:id"
+               :put (conj common-interceptors `generic-interceptors/auth `core/validate-id `core/upsert-provider `gen-events)
+               :route-name :provider-put]
+              ["/providers/:id/rate"
+               :put (conj common-interceptors `generic-interceptors/auth `core/validate-id `core/upsert-provider `gen-events)
+               :route-name :provider-rate]
+              ["/providers"
+               :post (conj common-interceptors `generic-interceptors/auth `core/validate `core/create-provider `gen-events)
+               :route-name :provider-post]
+              ["/providers/:id"
+               :delete (conj common-interceptors `generic-interceptors/auth `core/validate-id `core/delete-provider `gen-events)
+               :route-name :provider-delete]})
+
+(def service {:env :prod
+              ::http/routes routes
+              ::http/resource-path "/public"
+              ::http/type :immutant
+              ::http/port 8080})
