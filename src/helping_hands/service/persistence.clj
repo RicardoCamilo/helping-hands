d10eecac6749e5c26f6ba500fb017a3b9b5a4e2b
ricardo.machado <ricardo.machado@zup.com.br>
2020-05-25 21:30:51 -0300

    add core features for services

--- src/helping_hands/service/persistence.clj
+++ src/helping_hands/service/persistence.clj
@@ -1,1 +1,102 @@
-(ns helping-hands.service.persistence)
+(ns helping-hands.service.persistence
+  (:require [datomic.api :as d]))
+
+(defprotocol ServiceDB
+  "Abstraction for service database"
+
+  (upsert [this tx-data]
+    "Adds/Updates a service entity")
+
+  (entity [this id flds]
+    "Gets the specified service with all or requested fields")
+
+  (delete [this id]
+    "Deletes the specified service entity"))
+
+(defn- get-entity-id
+  [conn id]
+  (-> (d/q '[:find ?e
+             :in $ ?id
+             :where [?e :service/id ?id]] (d/db conn) (str id))
+      ffirst))
+
+(defn- get-entity
+  [conn id]
+  (let [eid (get-entity-id conn id)]
+    (->> (d/entity (d/db conn) eid) seq (into {}))))
+
+(defrecord ServiceDBDatomic [conn]
+  ServiceDB
+
+  (upsert [this {:keys [id type provider area cost rating status]}]
+    (d/transact conn
+                (vector (into {} (filter (comp some? val)
+                                         {:service/id id
+                                          :service/type type
+                                          :service/provider provider
+                                          :service/area area
+                                          :service/cost cost
+                                          :service/rating rating
+                                          :service/status status})))))
+
+  (entity [this id flds]
+    (when-let [service (get-entity conn id)]
+      (if (empty? flds)
+        service
+        (select-keys service (map keyword flds)))))
+
+  (delete [this id]
+    (when-let [eid (get-entity-id conn id)]
+      (d/transact conn [[:db.fn/retractEntity eid]]))))
+
+(defn create-service-database
+  "Creates a service database and returns the connection"
+  [d]
+  ;; create and connect to the database
+  (let [dburi (str "datomic:mem://" d)
+        db (d/create-database dburi)
+        conn (d/connect dburi)]
+    ;; transact schema if database was created
+    (when db
+      (d/transact conn
+                  [{:db/ident :service/id
+                    :db/valueType :db.type/string
+                    :db/cardinality :db.cardinality/one
+                    :db/doc "Unique Service Id"
+                    :db/unique :db.unique/identity
+                    :db/index true}
+                   {:db/ident :service/type
+                    :db/valueType :db.type/string
+                    :db/cardinality :db.cardinality/one
+                    :db/doc "Type of service"
+                    :db/index true
+                    :db/fulltext true}
+                   {:db/ident :service/provider
+                    :db/valueType :db.type/string
+                    :db/cardinality :db.cardinality/one
+                    :db/doc "Associated Service Provider ID"
+                    :db/index false}
+                   {:db/ident :service/area
+                    :db/valueType :db.type/long
+                    :db/cardinality :db.cardinality/many
+                    :db/doc "Service Areas / Locality"
+                    :db/index true
+                    :db/fulltext true}
+                   {:db/ident :service/cost
+                    :db/valueType :db.type/float
+                    :db/cardinality :db.cardinality/one
+                    :db/doc "Hourly Cost"
+                    :db/index false}
+                   {:db/ident :service/rating
+                    :db/valueType :db.type/float
+                    :db/cardinality :db.cardinality/many
+                    :db/doc "List of ratings"
+                    :db/index false}
+                   {:db/ident :service/status
+                    :db/valueType :db.type/string
+                    :db/cardinality :db.cardinality/one
+                    :db/doc "Status of Service (A/NA/D)"
+                    :db/index false}])
+      (ServiceDBDatomic. conn))
+    )
+  )
--- src/helping_hands/service/service.clj
+++ src/helping_hands/service/service.clj
@@ -1,1 +1,26 @@
-(ns helping-hands.service.service)
+(ns helping-hands.service.service
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.body-params :as body-params]))
+
+;; body-params converte de acordo com o content-type
+(def common-interceptors [(body-params/body-params) http/html-body])
+
+(def routes #{["/services/:id"
+               :get (conj common-interceptors `generic-interceptors/auth `core/validate-id `core/get-service `gen-events)
+               :route-name :service-get]
+              ["/services/:id"
+               :put (conj common-interceptors `generic-interceptors/auth `core/validate-id `core/upsert-service `gen-events)
+               :route-name :service-put]
+              ["/services/:id/rate"
+               :put (conj common-interceptors `generic-interceptors/auth `core/validate-id `core/upsert-service `gen-events)
+               :route-name :service-put]
+              ["/services"
+               :post (conj common-interceptors `generic-interceptors/auth `core/validate `core/upsert-service `gen-events)
+               :route-name :service-post]
+              ["/services/:id"
+               :delete (conj common-interceptors `generic-interceptors/auth `core/validate-id `core/upsert-service `gen-events)
+               :route-name :service-delete]})
+
+(def service {})
+
+
